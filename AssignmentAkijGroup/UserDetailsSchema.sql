198e9b232e6f136b9462204f6026a36fbea06e4e
Abrar051 <shahriar1410051@gmail.com>
2023-09-13 12:21:51 +0600

    Database and table Schema

---
+++ AssignmentAkijGroup/UserDetailsSchema.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserDetails] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [User]     NVARCHAR (50)  NULL,
+    [PassWord] NVARCHAR (MAX) NULL,
+    [Name]     NVARCHAR (MAX) NULL,
+    [Email]    NVARCHAR (50)  NULL,
+    [Address]  NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_UserDetails] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserDetails_UserDetails] FOREIGN KEY ([Id]) REFERENCES [dbo].[UserDetails] ([Id])
+);
